39aebf2b0fb0b1e6028a6d062aaf2ebff30724da
Jordan Ulmer <julmer526@gmail.com>
2023-04-08 11:18:47 +0100

    added styling to homepage

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,13 +1,30 @@
 import React from "react";
 import MenuCard from "./MenuCard";
+import styles from ".//componentStyles/HomeComponent.module.css"
 
 export default function HomeComponent() {
 
     return (
-        <div>
+        <div className={styles.container}>
             <MenuCard cardTitle="Matches"
-                      cardDescription="Check For New Matches"
-                      buttonText = "Go to Matches"  
+                      cardDescription="Check for new matches"
+                      buttonText = "Enter" 
+            />
+            <MenuCard cardTitle="Fosterer Applications"
+                      cardDescription="Review all fosterer applications"
+                      buttonText = "Enter" 
+            />
+            <MenuCard cardTitle="Adoption Applications"
+                      cardDescription="Review all adoption applications"
+                      buttonText = "Enter" 
+            />
+            <MenuCard cardTitle="Adoptee Applications"
+                      cardDescription="Review all adotpee applications"
+                      buttonText = "Enter" 
+            />
+            <MenuCard cardTitle="Add New Entity"
+                      cardDescription="Manually add a new person or animal"
+                      buttonText = "Enter"  
             />
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import HomeComponent from "@/components/HomeComponent"
 
 export default function Home() {
   return (
-    <div>
+    <div className="parentDiv">
      <Header />
      <HomeComponent />
     </div>
