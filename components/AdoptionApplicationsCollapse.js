d487a2a32d56b47e829b9a967dee3b2d7f72ddd6
Jordan Ulmer <julmer526@gmail.com>
2023-04-19 19:01:46 +0100

    added some detail to applications collapse

--- components/AdoptionApplicationsCollapse.js
+++ components/AdoptionApplicationsCollapse.js
@@ -9,8 +9,8 @@ export default function AdoptionApplicationsCollapse(props) {
             <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-5">
             <input type="checkbox" />
             <div className="collapse-title text-xl font-medium flex justify-around">
-                <span>{applicantName}</span>
-                <span>{dogName}</span>
+                <span>Applicant: {applicantName}</span>
+                <span>Dog: {dogName}</span>
                 <span>{applicationDate}</span>
             </div>
             <div className="collapse-content">
