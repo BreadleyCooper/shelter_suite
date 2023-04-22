edfaaa5e9a5874a66f94eeed7bdf6ec57e57b8b2
Jordan Ulmer <julmer526@gmail.com>
2023-04-22 13:20:32 +0100

    new branch for querying by area

--- components/AdoptionApplicationsCollapse.js
+++ components/AdoptionApplicationsCollapse.js
@@ -7,7 +7,7 @@ export default function AdoptionApplicationsCollapse(props) {
 
     return (
         <div>
-            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-5">
+            <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box my-5 mx-5">
             <input type="checkbox" />
             <div className="collapse-title text-xl font-medium flex justify-around">
                 <span>Applicant: {applicantName}</span>
